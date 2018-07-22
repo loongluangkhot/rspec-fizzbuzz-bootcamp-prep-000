cc260d7414cc40906aa580c1e42086be12954050
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-22 12:48:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,12 @@
 
 def fizzbuzz(num)
   if (num % 3 == 0) && (num % 5 == 0)
-    "fizzbuzz"
-  elsif num 
+    "Fizzbuzz"
+  elsif (num % 3 == 0)
+    "Fizz"
+  elsif (num % 5 == 0)
+    "Buzz"
+  else
+    nil
   end
 end
