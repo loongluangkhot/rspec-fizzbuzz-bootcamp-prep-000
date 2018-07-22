7488dcdfb3aae44db7ce4742daf225164dcf517e
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-22 12:51:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
+require 'pry'
+
 def fizzbuzz(num)
+  binding.pry
   if (num % 3 == 0) && (num % 5 == 0)
     "FizzBuzz"
   elsif (num % 3 == 0)
